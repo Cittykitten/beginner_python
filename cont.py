02e8854cb00da01fdd3360680b129ad742e8fc0f
Tegah Tiana <tegahtiana14@gmail.com>
2026-02-18 05:29:32 +0000

    Create cont.py

---
+++ cont.py
@@ -0,0 +1,15 @@
+# Nearly forgot string needed double quote""
+
+{
+    "John": {
+        "phone": "024xxxxxxx",
+        "email": "john@email.com"
+    }
+}
+
+{
+  "Cynthia" :{
+    "phone": "0534789278",
+    "email": "cynthiamensah@gmail.com"
+    }
+}
