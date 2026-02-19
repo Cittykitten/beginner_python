a9bbc8219111571981795c0fdda45991d2efb49f
Tegah Tiana <tegahtiana14@gmail.com>
2026-02-19 07:07:53 +0000

    Uncomment calculator operations in calc.py
    Uncomment arithmetic operations and print statements.

--- calc.py
+++ calc.py
@@ -26,16 +26,17 @@
 # operator = input("Enter the operator(+,-,/,*):")
 # num2 = input(int("Enter the second number":))
 #   if operator == "+":
-#      sum = num1 + num2; 
+#      sum = num1 + num2
 #      print("The sum of the two numbers is:", sum),
 #   elif operator =="-":
-#      diff = num1 - num2; 
+#      diff = num1 - num2
 #      print("The difference of the two numbers is:", diff),
 #   elif operator == "/":
-#         quotient = num1/num2; 
+#         quotient = num1/num2
 #         print("The quotient is:", quotient),
 #   elif operator == "*":
-#      prod = num1 * num2; Print("The product of the numbers is:", prod) 
+#        prod = num1 * num2
+#        print("The product of the numbers is:", prod) 
 #   else Print("Enter the right operator")
 
 
