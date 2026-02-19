3f576588d441423a6c968d7e620af10900023cc2
Tegah Tiana <tegahtiana14@gmail.com>
2026-02-19 07:06:37 +0000

    Fix comments in calculator code

--- calc.py
+++ calc.py
@@ -25,16 +25,16 @@
 # num1 = input(int("Enter the first number": ))
 # operator = input("Enter the operator(+,-,/,*):")
 # num2 = input(int("Enter the second number":))
-#   if operator == "+"
+#   if operator == "+":
 #      sum = num1 + num2; 
 #      print("The sum of the two numbers is:", sum),
-#   elif operator =="-";
+#   elif operator =="-":
 #      diff = num1 - num2; 
 #      print("The difference of the two numbers is:", diff),
-#   elif operator == "/";
+#   elif operator == "/":
 #         quotient = num1/num2; 
 #         print("The quotient is:", quotient),
-#   elif operator == "*";
+#   elif operator == "*":
 #      prod = num1 * num2; Print("The product of the numbers is:", prod) 
 #   else Print("Enter the right operator")
 
