add434fc0f8475f3a07f865523e18e6696b533d6
Tegah Tiana <tegahtiana14@gmail.com>
2026-02-19 07:04:59 +0000

    Fix calculator logic and input handling

--- calc.py
+++ calc.py
@@ -1,16 +1,49 @@
 # Simple Python Calculator
-num1 = input(int("Enter the first number": ))
-operator = input("Enter the operator(+,-,/,*):")
-num2 = input(int("Enter the second number":))
-  if operator = "+"
-     sum = num1 + num2; 
-     print("The sum of the two numbers is:", sum),
-  elif operator ="-";
-     diff = num1 - num2; 
-     print("The difference of the two numbers is:", diff),
-  elif operator = "/";
-       quotient = num1/num2; 
-       print("The quotient is:", quotient),
-  elif operator = "*";
-     prod = num1 * num2; Print("The product of the numbers is:", prod) 
-  else Print("Enter the right operator")
+# num1 = input(int("Enter the first number": ))
+# operator = input("Enter the operator(+,-,/,*):")
+# num2 = input(int("Enter the second number":))
+#   if operator = "+"
+#      sum = num1 + num2; 
+#      print("The sum of the two numbers is:", sum),
+#   elif operator ="-";
+#      diff = num1 - num2; 
+#      print("The difference of the two numbers is:", diff),
+#   elif operator = "/";
+#        quotient = num1/num2; 
+#        print("The quotient is:", quotient),
+#   elif operator = "*";
+#      prod = num1 * num2; Print("The product of the numbers is:", prod) 
+#   else Print("Enter the right operator")
+
+
+
+
+
+# Simple Python Calculator
+
+num1 = int(input("Enter the first number: "))
+operator = input("Enter the operator (+, -, *, /): ")
+num2 = int(input("Enter the second number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print("The sum of the two numbers is:", result)
+
+elif operator == "-":
+    result = num1 - num2
+    print("The difference of the two numbers is:", result)
+
+elif operator == "*":
+    result = num1 * num2
+    print("The product of the numbers is:", result)
+
+elif operator == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print("The quotient is:", result)
+    else:
+        print("Error: Division by zero is not allowed.")
+
+else:
+    print("Enter the right operator.")
+
