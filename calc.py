08fa74e68dc68a78dbdf15faa2bf275b821d8140
Tegah Tiana <tegahtiana14@gmail.com>
2026-02-19 07:05:28 +0000

    Fix and enable simple calculator functionality
    Uncommented and corrected the calculator code for functionality.

--- calc.py
+++ calc.py
@@ -19,6 +19,37 @@
 
 
 
+
+
+# Simple Python Calculator
+# num1 = input(int("Enter the first number": ))
+# operator = input("Enter the operator(+,-,/,*):")
+# num2 = input(int("Enter the second number":))
+#   if operator == "+"
+#      sum = num1 + num2; 
+#      print("The sum of the two numbers is:", sum),
+#   elif operator =="-";
+#      diff = num1 - num2; 
+#      print("The difference of the two numbers is:", diff),
+#   elif operator == "/";
+#         quotient = num1/num2; 
+#         print("The quotient is:", quotient),
+#   elif operator == "*";
+#      prod = num1 * num2; Print("The product of the numbers is:", prod) 
+#   else Print("Enter the right operator")
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Simple Python Calculator
 
 num1 = int(input("Enter the first number: "))
