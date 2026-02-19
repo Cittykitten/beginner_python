fef17a682bdeb3c9768413ab92669f63ae53c3e7
Tegah Tiana <tegahtiana14@gmail.com>
2026-02-19 06:58:54 +0000

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,12 @@
+# Simple Python Calculator
+num1 = input(int(Print ("Enter the first number": ))
+Operator = input(Print("Enter the operator(+-/*):")
+num2 = input(int(Print("Enter the second number":)))
+  sum = num1 + num2; Print("The sum of the two numbers is:" sum),
+  elif operator ="-";
+  diff = num1 - num2; Print("The difference of the two numbers is:" diff),
+  elif operator = "/";
+  quotient = num1/num2; Print("The quotient is:" quotient),
+  elif operator = "*";
+  prod = num1 * num2; Print("The product of the numbers is:" prod) 
+  else Print("Enter the right operator")
