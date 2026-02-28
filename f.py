2fb9cdddb5817b88948ac1dfcc481c46f43cdc27
Tegah Tiana <tegahtiana14@gmail.com>
2026-02-28 08:20:40 +0000

    Create f.py

---
+++ f.py
@@ -0,0 +1,10 @@
+name = "Tiana"
+age = 18
+
+print(f"My name is {name} and I am {age} years old.")
+print(f"Next year I will be {age + 1}.")
+
+
+# Output
+# My name is Tiana and I am 18 years old.
+# Next year I will be 19.
