b7e00eb299c1c5306a27af31aaf22dbc181278ae
Tegah Tiana <tegahtiana14@gmail.com>
2026-02-28 08:24:53 +0000

    Update f.py

--- f.py
+++ f.py
@@ -13,3 +13,7 @@ print(f"Next year I will be {age + 1}.")
 # Output
 # My name is Tiana and I am 18 years old.
 # Next year I will be 19.
+
+The output without the f would've been 
+# My name is {name} and I am {age} years old.
+# Next year I will be {age + 1}.
