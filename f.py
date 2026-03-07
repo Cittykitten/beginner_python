5650f3a3e06d0fe2f681aae1e8b0dc9d18b06ec9
Tegah Tiana <tegahtiana14@gmail.com>
2026-03-07 11:41:30 +0000

    Update f.py

--- f.py
+++ f.py
@@ -4,7 +4,7 @@
 # Anything inside {} must be a valid Python expression
 
 name = "Tiana"
-age = 18
+age = 17
 
 print(f"My name is {name} and I am {age} years old.")
 print(f"Next year I will be {age + 1}.")
@@ -12,8 +12,8 @@ print(f"Next year I will be {age + 1}.")
 
 
 # Output
-# My name is Tiana and I am 18 years old.
-# Next year I will be 19👌 
+# My name is Tiana and I am 17 years old.
+# Next year I will be 18👌 
 
 # The output without the f would've been 
 # My name is {name} and I am {age} years old.
